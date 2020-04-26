cd6463676e3d3964c04d6590ebd15fd380b73fcc
sarna <sarna.dev@protonmail.com>
2020-04-26 14:56:14 +0200

    Add build_embedded for performance boost

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Heimdallr.MixProject do
       app: :heimdallr,
       version: "0.1.0",
       elixir: "~> 1.10",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
