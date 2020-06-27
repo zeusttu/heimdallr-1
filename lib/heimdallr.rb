fd8dfa279aa90c383a7d6bba5a3f160387f4c710
sarna <sarna.dev@protonmail.com>
2020-06-27 13:08:05 +0200

    Add a command for displaying available roles

--- lib/heimdallr.rb
+++ lib/heimdallr.rb
@@ -5,8 +5,29 @@ require "discordrb"
 module Heimdallr
   bot = Discordrb::Commands::CommandBot.new token: ENV["DISCORD_BOT_TOKEN"], prefix: ","
 
-  bot.command(:ping, description: "Check if bot is responsive.", usage: "ping") do |event|
+  bot.command(:ping, description: "Check if bot is responsive.", usage: ",ping") do |event|
     "pong!"
+  end
+
+  bot.command(:roles, description: "Display available roles.", usage: ",roles") do |event|
+    tongue_twisters = [
+      "Fisker Frits fisker friske fisk",
+      "Var det Varde, hva? Var det, hva?",
+      "Storstrømsbrosekspropriationskommissionsmedlem",
+      "Hundrede pund hunpuddelhundeuld",
+      "Præstens ged i degnens eng",
+      "Bissens gipsbisps gipsgebis",
+      "Røde rødøjede rådne ørreder"
+    ]
+    <<~TEXT.strip
+      **List of available roles:**
+      **`Beginner`**: If you're just beginning to learn Danish (A1-A2). 
+      **`Intermediate`**: If you know enough to hold conversations comfortably (B1-B2).
+      **`Fluent`**: If you're able to pronounce "#{tongue_twisters.sample}" (C1-C2).
+      **`Native`**: Hvis du er dansk.
+      **`Ping Me For Lessons`**: If you want to get notified for ~weekly lessons.
+      **`Voice Chat`**: If you want to get notified for random voice-chats.
+    TEXT
   end
 
   bot.member_join do |event|
