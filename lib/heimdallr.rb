a0afa5c039b1b5d6dd75079ba5611c417138ab6b
sarna <sarna.dev@protonmail.com>
2020-11-08 12:30:16 +0100

    Add a barebones FAQ system

--- lib/heimdallr.rb
+++ lib/heimdallr.rb
@@ -1,9 +1,24 @@
 require "heimdallr/version"
 
 require "discordrb"
+require "sqlite3"
 
 module Heimdallr
   bot = Discordrb::Commands::CommandBot.new token: ENV["DISCORD_BOT_TOKEN"], prefix: ","
+
+  begin
+    db = SQLite3::Database.new "bot.db"
+    db.results_as_hash = true
+    db.execute <<-SQL
+      create table if not exists faq (
+        topic text,
+        description text
+      );
+    SQL
+  rescue SQLite3::Exception => exc
+    bot.log_exception exc
+    exit
+  end
 
   bot.command(:ping, description: "Check if bot is responsive.", usage: ",ping") do |event|
     "pong!"
@@ -49,6 +64,67 @@ module Heimdallr
 
   bot.message(start_with: "t!") do |event|
     event.respond "I'm terribly sorry, we don't have Tatsumaki here. Try `,help`."
+  end
+
+  bot.command(:faq, description: "Get a FAQ entry", usage: ",faq <topic>") do |event|
+    _command, topic = event.content.split
+    return "Please provide a topic. See `,help faq`" unless topic
+    begin
+      description = db.get_first_value "select description from faq where topic=?", topic
+      description || "Not found!"
+    rescue SQLite3::Exception => exc
+      bot.log_exception exc
+      "Oof ouch, an error occurred. Please let sarna know."
+    end
+  end
+
+  bot.command(
+    :faqadd,
+    description: "Set a FAQ entry (staff only)",
+    usage: ",faqadd <topic> <description>"
+  ) do |event|
+    staff = bot
+      .servers.values.first
+      .roles.select { |role|
+      ["Moderator", "Assistant Moderator", "Helper"].include? role.name
+    }
+    return "You're not staff!" unless event.author.roles.any? { |role| staff.include? role }
+
+    _command, topic, description = event.content.split(/\s+/, 3)
+    return "Please provide a topic. See `,help faqadd`" unless topic
+    return "Please provide a description. See `,help faqadd`" unless description
+    begin
+      db.execute "insert into faq values ( ?, ? )", topic, description
+      "Done :)"
+    rescue SQLite3::Exception => exc
+      bot.log_exception exc
+      "Oof ouch, an error occurred. Please let sarna know."
+    end
+  end
+
+  bot.command(
+    :faqrem,
+    description: "Remove a FAQ entry (staff only)",
+    usage: ",faqrem <topic>"
+  ) do |event|
+    staff = bot
+      .servers.values.first
+      .roles.select { |role|
+      ["Moderator", "Assistant Moderator", "Helper"].include? role.name
+    }
+    return "You're not staff!" unless event.author.roles.any? { |role| staff.include? role }
+
+    _command, topic = event.content.split
+    return "Please provide a topic. See `,help faqrem`" unless topic
+    begin
+      description = db.get_first_value "select description from faq where topic=?", topic
+      return "Not found!" unless description
+      db.execute "delete from faq where topic=?", topic
+      "Done :)"
+    rescue SQLite3::Exception => exc
+      bot.log_exception exc
+      "Oof ouch, an error occurred. Please let sarna know."
+    end
   end
 
   bot.run
